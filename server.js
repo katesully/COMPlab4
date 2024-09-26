096e30b459cbf065d3b788fd1ee7b267fc741566
Kate Sullivan <sullivan.kate.e@gmail.com>
2024-09-26 14:49:54 -0700

    all

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
+//LLMs were used in the creation of this code
+
 const express = require('express');
 const { getDate } = require('./modules/utils');
 const messages = require('./lang/en/string.json'); // Import messages from the en.js file
-const app = express();
-const port = process.env.PORT || 3000;
 
-// Define the GET route
-app.get('/COMP4537/labs/3/getDate/', (req, res) => {
-    const name = req.query.name || 'Guest'; // Get the name from query parameters
-    const currentTime = getDate(); // Get the current server time using the utility function
-    // Prepare the response message
-    const message = `<div style="color: blue;">${messages.greeting.replace('%name', name).replace('%time', currentTime)}</div>`;
-    // Send the response
-    res.send(message);
-});
+class App {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.setupRoutes();
+    }
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+    setupRoutes() {
+        // Define the GET route
+        this.app.get('/COMP4537/labs/3/getDate/', (req, res) => {
+            const name = req.query.name || 'Guest'; // Get the name from query parameters
+            const currentTime = getDate(); // Get the current server time using the utility function
+            // Prepare the response message
+            const message = `<div style="color: blue;">${messages.greeting.replace('%name', name).replace('%time', currentTime)}</div>`;
+            // Send the response
+            res.send(message);
+        });
+    }
+
+    start() {
+        // Start the server
+        this.app.listen(this.port, () => {
+            console.log(`Server is running at http://localhost:${this.port}`);
+        });
+    }
+}
+
+// Create an instance of the App class and start the server
+const appInstance = new App();
+appInstance.start();
